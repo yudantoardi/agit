620145f3e162deaf7008406525db7c00b907dcc6
yudantoardi <yudantoardi@gmail.com>
2021-11-15 10:23:21 +0700

    update pop up

--- asset/js/main.js
+++ asset/js/main.js
@@ -16,4 +16,20 @@ $(document).ready(function(){
         $($(this).attr("href")).addClass("show");
     });
 
+    // Select all »a« elements with a parent class »links« and add a function that is executed on click
+    $( '.my-nav li a' ).on( 'click', function(e){
+
+        $(".my-nav li").removeClass("active");
+        $($(this).parent()).addClass("active");
+        
+        var href = $(this).attr( 'href' );
+        
+        $( 'html, body' ).animate({
+            scrollTop: $( href ).offset().top
+        }, '300' );
+        
+        e.preventDefault();
+    
+    });
+
 });
